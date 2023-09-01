fad16bd3babcb8d036bd5b5fdc27861ae46ec06f
FrancoMarcolongo <francomarcolongo98@gmail.com>
2023-09-01 09:31:11 -0300

    fix Persona.compareTo

--- TAREA3_GRUPO11_LAB4/src/ejercicio1/Persona.java
+++ TAREA3_GRUPO11_LAB4/src/ejercicio1/Persona.java
@@ -4,15 +4,15 @@ public class Persona implements Comparable<Persona>{
 	
 	String nombre;
 	String apellido;
-	int dni;
+	String dni;
 	
 	public Persona() {
 		this.nombre = "Sin nombre";
 		this.apellido = "Sin apellido";
-		this.dni = 0;
+		this.dni = "";
 	}
 	
-	public Persona(String apellido, String nombre, int dni) {
+	public Persona(String apellido, String nombre, String dni) {
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.dni = dni;
@@ -24,35 +24,54 @@ public class Persona implements Comparable<Persona>{
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	@Override
+	public String toString() {
+		return apellido+"-"+nombre+"-"+dni;
+	}
+
 	public String getApellido() {
 		return apellido;
 	}
 	public void setApellido(String apellido) {
 		this.apellido = apellido;
 	}
-	public int getDni() {
+	public String getDni() {
 		return dni;
 	}
-	public void setDni(int dni) {
+	public void setDni(String dni) {
 		this.dni = dni;
 	}
 	
 	public static boolean verificarDniInvalido(String dni) throws DniInvalido
 	{
-	    for (int i = 0; i < dni.length(); i++) {
-	        char c = dni.charAt(i);
-	        if (c < '0' || c > '9') {
-	            throw new DniInvalido();
-	        }
-	    }
-	    return true;
+	    if( dni.matches("[0-9]+")) return true;
+	    throw new DniInvalido();
 	}
 
 	@Override
 	public int compareTo(Persona o) {
 		// TODO Auto-generated method stub
-		return this.apellido.compareToIgnoreCase(o.getApellido());
+		return this.toString().compareToIgnoreCase(o.toString());
 	}
 	
 	
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- TAREA3_GRUPO11_LAB4/src/ejercicio1/Principal.java
+++ TAREA3_GRUPO11_LAB4/src/ejercicio1/Principal.java
@@ -2,14 +2,19 @@ package ejercicio1;
 
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.ListIterator;
 import java.util.TreeSet;
 
 public class Principal {
 
 	public static void main(String[] args) {
-		/*
+		
+		//punto A
 		try {
 			Persona.verificarDniInvalido("41544556");
 			System.out.println("dni verificado");
@@ -23,25 +28,26 @@ public class Principal {
 		} catch (DniInvalido e) {
 			System.out.println(e.getMessage());
 		}
-		*/
+		
 		
 		//punto B
 		FileReader input;
 		BufferedReader buffer;
+		TreeSet<Persona> list = new TreeSet<Persona>();
 		
 		try {
 			input = new FileReader("Personas.txt");
 			buffer = new BufferedReader(input);
 			String linea = buffer.readLine();
 
-			TreeSet<Persona> list = new TreeSet<Persona>();
+			
 			
 			while(linea != null){
 				String[] aux = linea.split("-");
 				if(aux[0] != ""){
 					try {
 						Persona.verificarDniInvalido(aux[2]);
-						Persona auxP = new Persona(aux[0],aux[1],Integer.parseInt(aux[2]));
+						Persona auxP = new Persona(aux[1],aux[0],aux[2]);
 						list.add(auxP);
 					}catch(DniInvalido e) {
 						System.out.println(e.getMessage());
@@ -56,7 +62,25 @@ public class Principal {
 			e.printStackTrace();
 		}
 	
-	
+		//punto C
+		try {
+            FileWriter output = new FileWriter("Resultado.txt");
+            BufferedWriter writer = new BufferedWriter(output);
+            
+            ListIterator<Persona> it = new ArrayList<>(list).listIterator();
+
+            while (it.hasNext()) {
+                Persona persona = it.next();
+                writer.write(persona.toString());
+                writer.newLine();
+            }
+
+            writer.close();
+            output.close();
+            System.out.println("Personas guardadas en Resultado.txt");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 	}
 
 
